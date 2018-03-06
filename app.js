f25879c9112f25e84bbad1ae895da7f310a41923
BuildTools <unconfigured@null.spigotmc.org>
2018-03-05 21:25:04 -0800

    asdrgyrg

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 express = require('express');
 app = express();
 
+http = require('http')
+HTTP_PORT = 80;
+HTTPS_PORT = 443;
+
 var http = require('https')
 var fs = require('fs')
 
@@ -11,10 +15,26 @@ var options = {
 	cert: fs.readFileSync(sslPath + 'fullchain.pem')
 }
 
+app.set('port', HTTP_PORT);
+
+app.all('/*', function(req, res, next) {
+  if (/^http$/.test(req.protocol)) {
+    var host = req.headers.host.replace(/:[0-9]+$/g, ""); // strip the port # if any
+    if ((HTTPS_PORT != null) && HTTPS_PORT !== 443) {
+      return res.redirect("https://" + host + ":" + HTTPS_PORT + req.url, 301);
+    } else {
+      return res.redirect("https://" + host + req.url, 301);
+    }
+  } else {
+    return next();
+  }
+});
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
+	console.log("Someone Connected")
 	res.render("homepage");
 });
 
