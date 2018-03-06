5e398ea0d38d9fbc46b1d81d4c19d8ddca836b65
BuildTools <unconfigured@null.spigotmc.org>
2018-03-05 19:43:32 -0800

    asdrjgtjdhr

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require('express');
 app = express();
+
+var http = require('https')
+var fs = require('fs')
+
+var sslPath = '/etc/letsencrypt/live/www.jaxtubbs.com/'
+
+var options = {
+	key: fs.readFileSync(sslPath + 'privkey.pem'),
+	cert: fs.readFileSync(sslPath + 'fullchain.pem')
+}
+
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
 	res.render("homepage");
 });
+
+app.get('/health-check',(req,res) => res.sendStatus(200));
+
+
 app.get("/websites", function(req,res){
 	res.render("websites");
 });
@@ -20,6 +36,6 @@ app.get("/parlax-photography", function(req,res){
 	res.render("websites/paralaxPhotography");
 });
 
-app.listen(process.env.PORT || 80, function(){
+app.listen(443, function(){
 	console.log("Starting Server");
 });
