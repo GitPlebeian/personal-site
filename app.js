2fa90d4ef4018eebc7351fc06ef587afa7fc5f57
BuildTools <unconfigured@null.spigotmc.org>
2018-03-06 22:42:21 -0800

    commited

--- app.js
+++ app.js
@@ -18,19 +18,6 @@ app.get("/", function(req,res){
 	console.log("Someone Connected")
 	res.render("homepage");
 });
-app.get("/websites", function(req,res){
-	res.render("websites");
-});
-app.get("/contact", function(req,res){
-	res.render("contact");
-});
-
-app.get("/single-page", function(req,res){
-	res.render("websites/onePage");
-});
-app.get("/parlax-photography", function(req,res){
-	res.render("websites/paralaxPhotography");
-});
 
 server = http.createServer(options, app)
 io = require('socket.io').listen(server)
