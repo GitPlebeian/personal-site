25a41743ed109966599c0559fd2fbe872f22e272
BuildTools <unconfigured@null.spigotmc.org>
2018-03-11 22:44:16 -0700

    commited

---
+++ public/scripts/business.js
@@ -0,0 +1,16 @@
+function navBar() {
+    var x = document.getElementById("navBar");
+    if (x.className === "navBar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navBar";
+    }
+}
+function focusContent() {
+  var x = document.getElementById("navBar");
+  if (x.className === "navBar") {
+    
+  } else {
+      x.className = "navBar";
+  }
+}
