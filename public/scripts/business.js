cb15c4abb60e80530549737172e14efba92e68f7
BuildTools <unconfigured@null.spigotmc.org>
2018-03-15 16:50:54 -0700

    commited

--- public/scripts/business.js
+++ public/scripts/business.js
@@ -1,15 +1,18 @@
-function navBar() {
+function navBar(navBar) {
     var x = document.getElementById("navBar");
+    var y = document.getElementById("navBarSlideInid");
     if (x.className === "navBar") {
         x.className += " responsive";
     } else {
         x.className = "navBar";
     }
+    navBar.classList.toggle("navIconBarActive");
+    y.classList.toggle("navBarSlideInActive");
 }
 function focusContent() {
   var x = document.getElementById("navBar");
   if (x.className === "navBar") {
-    
+
   } else {
       x.className = "navBar";
   }
