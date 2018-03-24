efa25ee31a54c2f92b2d57be9fd47a0db063c272
BuildTools <unconfigured@null.spigotmc.org>
2018-03-24 08:33:16 -0700

    commited

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -9,14 +9,24 @@ $('.btn').click( function(event) {
 
 function navBar(navBar) {
     var x = document.getElementById("navBar");
-    var y = document.getElementById("navBarSlideInid");
+    var y = $('#navBarSlideInid');
     if (x.className === "navBar") {
         x.className += " responsive";
+        y.css('display', 'block')
+        y.animate({
+          top: '52px'
+        }, 200)
     } else {
         x.className = "navBar";
+        y.animate({
+          top: '-52px'
+        }, 200)
+        setTimeout( function () {
+            y.css('display', 'none')
+        }, 200);
     }
     navBar.classList.toggle("navIconBarActive");
-    y.classList.toggle("navBarSlideInActive");
+    y.toggleClass("navBarSlideInActive");
 }
 function focusContent() {
   var x = document.getElementById("navBar");
