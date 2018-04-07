9f518a717ebc6192aa9960d881a54c16634df8b4
BuildTools <unconfigured@null.spigotmc.org>
2018-04-07 07:05:41 -0700

    commited

--- redirect.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-app = express();
-
-app.get('*', function(req, res) {
-    res.redirect('https://' + req.headers.host + req.url);
-})
-
-app.listen(80, function(){
-});
