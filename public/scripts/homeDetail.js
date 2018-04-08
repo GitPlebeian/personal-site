48fbe238dd7d132d8309049ad3915238032307a2
BuildTools <unconfigured@null.spigotmc.org>
2018-04-08 16:13:15 -0700

    commited

--- public/scripts/homeDetail.js
+++ public/scripts/homeDetail.js
@@ -5,3 +5,18 @@ function navBar(){
   navBarToggle.toggleClass("active")
   navBarUL.toggleClass('active')
 }
+
+function imgPullOut(img){
+  jimg = $(img)
+
+  jimg.toggleClass("active")
+}
+
+function accordianToggle(obj) {
+  obj = obj.nextElementSibling;
+  if(obj.style.maxHeight){
+    obj.style.maxHeight = null
+  } else {
+    obj.style.maxHeight = obj.scrollHeight + 'px'
+  }
+}
