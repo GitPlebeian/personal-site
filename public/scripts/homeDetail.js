85770340858ff3aefc5ad7770fd4b2d86f16e08f
BuildTools <unconfigured@null.spigotmc.org>
2018-04-09 05:56:06 -0700

    commited

--- public/scripts/homeDetail.js
+++ public/scripts/homeDetail.js
@@ -1,22 +1,32 @@
 function navBar(){
   navBarToggle = $('.navBarToggle')
-  navBarUL = $('.navBarUL')
+  obj = document.getElementById("navBarLinks")
 
+  $(obj).css('color',"red")
   navBarToggle.toggleClass("active")
-  navBarUL.toggleClass('active')
-}
-
-function imgPullOut(img){
-  jimg = $(img)
-
-  jimg.toggleClass("active")
-}
-
-function accordianToggle(obj) {
-  obj = obj.nextElementSibling;
   if(obj.style.maxHeight){
     obj.style.maxHeight = null
   } else {
     obj.style.maxHeight = obj.scrollHeight + 'px'
+  }
+}
+
+
+function accordianToggle(thing) {
+  things = thing.nextElementSibling
+  obj = $(thing.nextElementSibling);
+  allAcorrdions = $('.accordianContent')
+  allAcorrdions.each(function(){
+    $(this).css('max-height','0')
+  })
+  if(things.style.maxHeight >= 20){
+    obj.css('max-height','0')
+    setTimeout(function(){
+      obj.css('border-width','0px')
+    },205)
+  } else {
+    console.log('asdf');
+    obj.css('border-width','1px')
+    things.style.maxHeight = things.scrollHeight + 'px'
   }
 }
