b5f754559c9cf0bb46a5d4e1624ada39926fc419
BuildTools <unconfigured@null.spigotmc.org>
2018-04-10 16:25:50 -0700

    commited

--- public/scripts/homeDetail.js
+++ public/scripts/homeDetail.js
@@ -13,19 +13,27 @@ function navBar(){
 
 
 function accordianToggle(thing) {
+  var changeToDeactive = false;
   things = thing.nextElementSibling
   obj = $(thing.nextElementSibling);
+  if($(thing).hasClass('active')){
+    changeToDeactive = true
+  }
+  $('.accordianToggle').removeClass('active')
+
+  if(!changeToDeactive){
+    $(thing).toggleClass('active')
+  }
   allAcorrdions = $('.accordianContent')
   allAcorrdions.each(function(){
     $(this).css('max-height','0')
   })
-  if(things.style.maxHeight >= 20){
+  if(changeToDeactive){
     obj.css('max-height','0')
     setTimeout(function(){
       obj.css('border-width','0px')
     },205)
   } else {
-    console.log('asdf');
     obj.css('border-width','1px')
     things.style.maxHeight = things.scrollHeight + 'px'
   }
