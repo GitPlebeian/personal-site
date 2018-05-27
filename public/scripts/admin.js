4ab0846cfb16eaaab13de50e7bbff8cdf8e6440a
Jax <Jaxtubbs@gmail.com>
2018-05-27 14:31:59 -0700

    as;ldkjr

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -6,15 +6,15 @@ $('.button').click(function() {
 		'type': 'GET',
 		'success': function(rawData) {
 			for (var i = 0; i < rawData.length; i++) {
-				console.log('RawDataLoop: ' + i)
+				// console.log('RawDataLoop: ' + i)
 				var alreadyInArray = true
 				for (var k = 0; k < differentUsers.length; k++) {
-					console.log('differentUsers: ' + k)
-					console.log(rawData[i].ip)
-					console.log(differentUsers[k].ip)
+					// console.log('differentUsersLoop: ' + k)
+					// console.log(rawData[i].ip + ' RawData Ip')
+					// console.log(differentUsers[k].ip + ' differentUsers Ip')
 
 					if (rawData[i].ip == differentUsers[k].ip) {
-						console.log('user:' + rawData[i].ip + ' already in array')
+						// console.log('user:' + rawData[i].ip + ' already in array')
 						alreadyInArray = false
 						differentUsers[k].numTimes += 1
 					}
@@ -23,8 +23,10 @@ $('.button').click(function() {
 					differentUsers.push(rawData[i])
 				}
 			}
-			console.log(differentUsers)
-
+			// console.log(differentUsers)
+			for (var i = 0; i < differentUsers.length; i++) {
+				$('ul.data').append('<li>' + differentUsers[i].numTimes + ': ' + differentUsers[i].ip + '</li>')
+			}
 		},
 		'error': function(request, error) {
 			alert("Request: " + JSON.stringify(request));
