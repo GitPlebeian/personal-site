4d1e07c4e5a36c0e36804a1ec82d705af39044af
Jax <Jaxtubbs@gmail.com>
2018-05-27 14:17:48 -0700

    as;ldkjr

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,11 +1,29 @@
 $('.button').click(function() {
+	var differentUsers = []
 	$.ajax({
 
 		'url': '/trafficData',
 		'type': 'GET',
-		'success': function(data) {
-			console.log(data)
-			$('p.data').html(data + " Is Data");
+		'success': function(rawData) {
+			for (var i = 0; i < rawData.length; i++) {
+				console.log('RawDataLoop: ' + i)
+				var alreadyInArray = true
+				for (var k = 0; k < differentUsers.length; k++) {
+					console.log('differentUsers: ' + k)
+					console.log(rawData[i].ip)
+					console.log(differentUsers[k].ip)
+
+					if (rawData[i].ip == differentUsers[k].ip) {
+						console.log('user:' + rawData[i].ip + ' already in array')
+						alreadyInArray = false
+						differentUsers[k].numTimes += 1
+					}
+				}
+				if (alreadyInArray) {
+					differentUsers.push(rawData[i])
+				}
+			}
+			console.log(differentUsers)
 
 		},
 		'error': function(request, error) {
